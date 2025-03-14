eff2f519b63dc831028e3a89bfb027ae5021ddee
Vishwa <vishwajitsayambar92@gmail.com>
2025-03-15 00:24:29 +0530

    Updated Order and Product Model - If we have to use factory in modules then we have to tell that this is model in factory - then we can use in test like ProductFactory::new()->create() - But when we try to use like Order::factory()->create(); then it will not work as - Bydefault it checks in normal Databse/factories path but we have at different location - so we have to override the newFactory method in Model

--- modules/Order/Models/Order.php
+++ modules/Order/Models/Order.php
@@ -2,9 +2,18 @@
 
 namespace Modules\Order\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Order\Database\Factories\OrderFactory;
 
 class Order extends Model
 {
+    use HasFactory;
+    const STATUS_PENDING = 'pending';
+    const STATUS_PROCESSING = 'processing';
 
+    protected static function newFactory(): OrderFactory
+    {
+        return OrderFactory::new();
+    }
 }
--- modules/Product/Models/Product.php
+++ modules/Product/Models/Product.php
@@ -2,9 +2,11 @@
 
 namespace Modules\Product\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
 
+    use HasFactory;
 }
